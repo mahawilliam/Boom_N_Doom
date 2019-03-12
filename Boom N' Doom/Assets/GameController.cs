35dc9cf298402705ac8394ebf594f0381410d0f7
mahawilliam <mahawilliam@users.noreply.github.com>
2019-03-12 07:43:52 +0700

    add Game Controller (not finished)

---
+++ Boom N' Doom/Assets/GameController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public Sprite[] cannonSprites;
+    public float startWait;
+    public float cannonWait;
+    public GameObject cannon1;
+    public GameObject bombRed;
+    void Start()
+    {
+        StartCoroutine (SpawnWaves ());   
+    }
+
+    IEnumerator SpawnWaves ()
+    {
+        
+        while (true) {
+            for (int i = 0; i < cannonSprites.Length; i++) {
+                cannon1.GetComponent<SpriteRenderer>().sprite = cannonSprites[i];
+                yield return new WaitForSeconds (cannonWait);
+                if (i == cannonSprites.Length - 1) {
+                    ShootBomb();
+                }
+                
+            }
+        }
+    }
+
+    void ShootBomb() {
+        GameObject bombClone = Instantiate(bombRed, cannon1.transform.position, Quaternion.identity);
+        bombClone.GetComponent<Rigidbody2D>().velocity = transform.right * Random.Range(-10f, 10f);
+    }
+}
