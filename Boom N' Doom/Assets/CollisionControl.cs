aceffdddb494c365c8d56ea8f78877e732f05776
mahawilliam <mahawilliam@users.noreply.github.com>
2019-03-12 07:44:05 +0700

    add collision mechanics for bomb

---
+++ Boom N' Doom/Assets/CollisionControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.tag == "Player" && GetComponent<Rigidbody2D>() != null) {
+            
+        }
+        else if (other.gameObject.tag == "Tile"){
+            Destroy(GetComponent<Rigidbody2D>());
+        }
+    }
+}
