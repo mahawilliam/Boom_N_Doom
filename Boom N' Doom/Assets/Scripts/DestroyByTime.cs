1a32e4d9adae6ac7f386931eccb5c19e214fe203
mahawilliam <mahawilliam@users.noreply.github.com>
2019-03-18 14:44:12 +0700

    Refactoring git

---
+++ Boom N' Doom/Assets/Scripts/DestroyByTime.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByTime : MonoBehaviour
+{
+    public float lifetime;
+    public ParticleSystem explosion;
+   
+    void Start ()
+    {
+        Destroy (gameObject, lifetime);
+    }
+
+    void OnDestroy() {
+        Explode();    
+        //CameraShake();
+    }
+
+    void Explode() {
+        Instantiate( explosion, transform.position, Quaternion.identity );
+    }
+    /* 
+    public Transform camTransform;
+    public float shakeAmount = 0.7f;
+    public float shakeDuration = 0f;
+	public float decreaseFactor = 1.0f;
+    Vector2 originalPos;
+    void CameraShake() {
+        while (shakeDuration > 0) {
+            originalPos = camTransform.localPosition;
+            camTransform.localPosition = originalPos + Random.insideUnitCircle * shakeAmount;
+			
+			shakeDuration -= Time.deltaTime * decreaseFactor;
+        }
+        camTransform.localPosition = originalPos;
+    }
+    */
+}
